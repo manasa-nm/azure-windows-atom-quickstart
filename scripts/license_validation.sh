6860e165b48e7fd29151e90df436f9a69b09bd12
Ganesh-Yeole <92855225+Ganesh-Yeole@users.noreply.github.com>
2022-05-25 10:42:20 +0530

    Create license_validation.sh

---
+++ scripts/license_validation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/OfficialBoomi/azure-atom-quickstart/main/scripts/requirements.txt
+wget https://raw.githubusercontent.com/OfficialBoomi/azure-atom-quickstart/main/scripts/license_validation.py
+
+pip install -t . -r ./requirements.txt
+
+if [ $BOOMIAUTHENTICATIONTYPE == "Token" ]
+then
+    result=`python license_validation.py "$ATOMACCOUNTID" "BOOMI_TOKEN.$ATOMUSERNAME" "$BOOMIMFAAPITOKEN" ATOM 60`
+else
+    result=`python license_validation.py "$ATOMACCOUNTID" "$ATOMUSERNAME" "$ATOMPASSWORD" ATOM 60`
+fi
+
+status=`echo $result|cut -d, -f1|awk -F':' '{ print $2 }'`
+token=`echo $result|cut -d, -f2|awk -F':' '{ print $2 }'`
+
+echo \{\"license_validation\":\"$status\"\, \"installation_token\":\"$token\"\} > $AZ_SCRIPTS_OUTPUT_PATH
